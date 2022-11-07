0919de837e99a8794dbd4392e859d0fdde02e325
AswathiMohan23 <achu999here@gmail.com>
2022-11-07 22:55:28 +0530

    [ADDED] : code to create a binary tree as per the diagram and find its size

--- src/com/java/MyBinaryTree.java
+++ src/com/java/MyBinaryTree.java
@@ -14,10 +14,31 @@ public class MyBinaryTree<K extends Comparable<K>> {
         MyBinaryNode first = new MyBinaryNode(56);
         MyBinaryNode second = new MyBinaryNode(30);
         MyBinaryNode third = new MyBinaryNode(70);
+        MyBinaryNode fourth = new MyBinaryNode(22);
+        MyBinaryNode fifth = new MyBinaryNode(40);
+        MyBinaryNode sixth = new MyBinaryNode(11);
+        MyBinaryNode seventh = new MyBinaryNode(3);
+        MyBinaryNode eight = new MyBinaryNode(16);
+        MyBinaryNode nine = new MyBinaryNode(60);
+        MyBinaryNode ten = new MyBinaryNode(95);
+        MyBinaryNode eleven = new MyBinaryNode(65);
+        MyBinaryNode twelve = new MyBinaryNode(63);
+        MyBinaryNode thirteen = new MyBinaryNode(67);
+
         root = first; // first value will be assigned to root
         first.left = second; //second will be assigned to left of first
         first.right = third;//third will be assigned to right of first
-        // second----first----third   ie, 30-----56----70
+        second.left=fourth;
+        second.right=fifth;
+        fourth.left=sixth;
+        sixth.left=seventh;
+        sixth.right=eight;
+        third.left=nine;
+        third.right=ten;
+        nine.right=eleven;
+        eleven.left=twelve;
+        eleven.right=thirteen;
+
         int size=getSizeOfTree(root);
         System.out.println("the size of the tree : "+size);
 
